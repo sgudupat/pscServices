55cb1a6ee65b0f4b15f6e3a90ebdd7001cfca4af
sgudupat <sandeep.g@pscglobalsolutions.com>
2014-11-19 16:14:43 +0530

    Fixed alignment issues.

--- src/main/java/pscapp/services/dao/ContactsDaoImpl.java
+++ src/main/java/pscapp/services/dao/ContactsDaoImpl.java
@@ -1,30 +1,27 @@
 package pscapp.services.dao;
 
-import java.util.List;
-
 import pscapp.services.types.ContactInfo;
+
+import java.util.List;
 
 public class ContactsDaoImpl implements ContactsDao {
 
-	public List<ContactInfo> getContacts(String email, String password) {
+    public List<ContactInfo> getContacts(String email, String password) {
+        if (authenticateUserLogin(email, password)) {
+        }
+        return null;
+    }
 
-		if(authenticateUserLogin(email, password)){
+    private boolean authenticateUserLogin(String email, String password) {
+        boolean result = false;
+        int count = -1;
+        int count2 = -1;
+        String validEmailPasswordQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND password = ? ";
+        String accountActiveQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND status = 'ACTIVE' ";
+        //QueryParameters params = new QueryParameters();
+        //params.addParam(email);
+        //params.addParam(password);
 
-		}
-		return null;
-	}
-
-	private boolean authenticateUserLogin(String email, String password) {
-		boolean result = false;
-		int count = -1;
-		int count2 = -1;
-		String validEmailPasswordQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND password = ? ";
-		String accountActiveQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND status = 'ACTIVE' ";
-		//QueryParameters params = new QueryParameters();
-		//params.addParam(email);
-		//params.addParam(password);
-
-		return false;
-	}	
-
+        return false;
+    }
 }
