caa4b73c7443e2f5556554f9dc692da8d8d0597a
murali333 <murali.n333@gmail.com>
2014-11-19 12:36:53 +0530

    Changes are made

--- src/main/java/pscapp/services/dao/ContactsDaoImpl.java
+++ src/main/java/pscapp/services/dao/ContactsDaoImpl.java
@@ -18,8 +18,8 @@ public class ContactsDaoImpl implements ContactsDao {
 		boolean result = false;
 		int count = -1;
 		int count2 = -1;
-
-		String validEmailPasswordQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND password = ? ";
+		//removing password from query
+		String validEmailPasswordQuery = "SELECT COUNT(*) FROM USERS WHERE email = ?  ";
 		String accountActiveQuery = "SELECT COUNT(*) FROM USERS WHERE email = ? AND status = 'ACTIVE' ";
 		//QueryParameters params = new QueryParameters();
 		//params.addParam(email);
